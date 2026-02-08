db1fd69b823bc2e9046737aba6188268bf96c63e
SignaturePrintz <2ulayman.com@gmail.com>
2026-02-08 22:18:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-const instagramBtn = document.getElementById("instagramBtn");
+document.getElementById("instagramBtn").onclick = function () {
+  window.open("https://www.instagram.com/signatureprintz_345", "_blank");
+};
 
-instagramBtn.addEventListener("click", () => {
-  window.open(
-    "https://www.instagram.com/signatureprintz_345",
-    "_blank"
-  );
-});
-
-// Facebook button
-const facebookBtn = document.getElementById("facebookBtn");
-
-facebookBtn.addEventListener("click", () => {
+document.getElementById("facebookBtn").onclick = function () {
   window.open(
     "https://www.facebook.com/profile.php?id=61587441433646",
     "_blank"
   );
-});
+};
+
