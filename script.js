0b61625a6c221c76561bb0b9e04c579131267ac3
SignaturePrintz <2ulayman.com@gmail.com>
2026-02-08 21:44:22 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const instagramBtn = document.getElementById("instagramBtn");
+
+instagramBtn.addEventListener("click", () => {
+  window.open(
+    "https://www.instagram.com/signatureprintz_345",
+    "_blank"
+  );
+});
+
+// Facebook button
+const facebookBtn = document.getElementById("facebookBtn");
+
+facebookBtn.addEventListener("click", () => {
+  window.open(
+    "https://www.facebook.com/profile.php?id=61587441433646",
+    "_blank"
+  );
+});
